1e413e0d5bae3230045a69b912a80ed0a8581e8a
Agustin Rangel <agustinran.ib@gmail.com>
2021-09-30 22:51:16 -0500

    Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
-import Homepage from './Pages/Homepage/Homepage.component.jsx';
+import "./App.css";
+import Homepage from "./Pages/Homepage/Homepage.component.jsx";
+import { Switch, Route } from "react-router-dom";
 
+const HatsPage = () => (
+	<div>
+		<h1>Hats Page</h1>
+	</div>
+);
 
 function App() {
 	return (
 		<div className="App">
-			<Homepage/>
+			<Switch>
+				<Route exact path="/" component={Homepage} />
+				<Route path="/hats" component={HatsPage} />
+			</Switch>
 		</div>
 	);
 }
